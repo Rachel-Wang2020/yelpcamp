c1837157d2cf7d7699840f14f44508955f09ca66
Rachel-Wang2020 <ruiqiwang1018@gmail.com>
2025-01-09 21:40:08 -0800

    campground edit & update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 const Campground = require("./models/campground");
 
@@ -20,6 +21,7 @@ app.set("views", path.join(__dirname, "views"));
 
 //这行代码通常在您需要接收通过 POST 或 PUT 请求提交的表单数据时使用
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -52,6 +54,16 @@ app.get("/campgrounds/:id", async (req, res) => {
   res.render("campgrounds/show", { campground });
 });
 
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
 app.listen(3000, () => {
   console.log("Serving on port 3000");
 });
